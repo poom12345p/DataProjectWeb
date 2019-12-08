c623d2c46ad1bbb1d7ba230d7591acc8ea423169
fsuriya <flukbanrong@gmail.com>
2019-12-09 02:11:01 +0700

    Update account.js

--- js/page/account.js
+++ js/page/account.js
@@ -9,7 +9,8 @@ if (sessionStorage.getItem('USER')) {
     {
       location.replace(location.origin + "/login")
     }
-list03.innerHTML=`<a href="http://localhost:9000/employeeInfo?employeeNumber=${username.employeeNumber}">${username.firstName} ${username.lastName}</a>`;
+list03.innerHTML=`<a href="http://localhost:9000/employeeInfo?employeeNumber=${username.employeeNumber}">${username.firstName} ${username.lastName}</a>
+                  <a>${username.jobTitle}</a>`;
 console.log(username.firstName);
 
 list04.innerHTML=`<a class="fa fa-user" href="http://localhost:9000/employeeInfo?employeeNumber=${username.employeeNumber}"> Account Info</a>`;
