3e41f393a7a2ad10d772e80c76ba15d783492b7f
ravihashi <ravicha_hashimoto@cmu.ac.th>
2019-12-09 01:10:54 +0700

    check duplicate code (product/promotion)

--- js/page/promotionregis.js
+++ js/page/promotionregis.js
@@ -11,27 +11,42 @@ $(document).ready(function () {
       expire: $("#expire").val()
     }
     $.ajax({
-      type: "POST",
-      url: "/createpromotion",
-      data: data,
-      dataType: "json",
-      success: (promo) => {
-        console.log("promotion success");
-        //   if (localStorage.getItem('contacts') === null) {
-        //     var contacts = [];
-        //     contacts.push(myContact);
-        //     localStorage.setItem('contacts', JSON.stringify(contacts));
-        //   } else {
-        //     var contacts = JSON.parse(localStorage.getItem('contacts'));
-        //     contacts.push(myContact);
-        //     localStorage.setItem('contacts', JSON.stringify(contacts));
-        //   }
-        // sessionStorage.setItem('PROMOTION', JSON.stringify(promo));
-        // location.replace(location.origin + `/productslist`);
-      },
-
-
-    });
+      // all URLs are relative to http://localhost:3000/
+      url: "/search/promotions",
+      type: 'GET',
+      dataType: 'json', // this URL returns data in JSON format
+      success: (promotions) => {
+        console.log("promotion number " + promotions.length);
+        promotions.forEach(promotion => {
+          if (promotion.code == data.code) {
+            console.log("Duplicate promotion code");
+            alert("Duplicate promotion code");
+            return 0;
+          }
+        });
+      }
+    }).always(function () {
+      $.ajax({
+        type: "POST",
+        url: "/createpromotion",
+        data: data,
+        dataType: "json",
+        success: (promo) => {
+          console.log(promo);
+          //   if (localStorage.getItem('contacts') === null) {
+          //     var contacts = [];
+          //     contacts.push(myContact);
+          //     localStorage.setItem('contacts', JSON.stringify(contacts));
+          //   } else {
+          //     var contacts = JSON.parse(localStorage.getItem('contacts'));
+          //     contacts.push(myContact);
+          //     localStorage.setItem('contacts', JSON.stringify(contacts));
+          //   }
+          // sessionStorage.setItem('PROMOTION', JSON.stringify(promo));
+          // location.replace(location.origin + `/productslist`);
+        }
+      });
+    })
   });
 });
 // const path = require('path');
