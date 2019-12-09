f4819c2f8461fd35e2e684f1ca834e8919c9b987
fsuriya <flukbanrong@gmail.com>
2019-12-09 12:01:17 +0700

    update payment

---
+++ js/page/menu_bar.js
@@ -0,0 +1,95 @@
+let menubaritem = document.querySelector('.menubaritem');
+let menubaritem2 = document.querySelector('.menubaritem2');
+
+menubaritem.innerHTML = `<nav class="main-nav main-nav4">
+<ul>
+    <li class="menu-item-has-children">
+        <a
+            href="/">Home</a>
+    </li>
+    <li class="has-mega-menu">
+        <a
+            href="ProductLotList.html">PRODUCT</a>
+    </li>
+    <li class="has-mega-menu">
+            <a
+                href="Order_list.html">ORDER</a>
+    </li>
+    <li class="has-mega-menu">
+            <a
+                href="CustomerList.html">CUSTOMER</a>
+    </li>
+    <li class="has-mega-menu">
+            <a
+                href="EmployeeList.html">EMPLOYEE</a>
+    </li>
+    <li class="has-mega-menu">
+        <a
+            href="promotionslist.html">Promotions</a>
+    </li>
+    <li class="has-mega-menu">
+        <a
+            href="promotionslist.html">Payment</a>
+    </li></nav>`;
+
+menubaritem.innerHTML = `<nav class="main-nav main-nav4">
+<ul>
+    <li class="menu-item-has-children">
+        <a
+            href="/">Home</a>
+    </li>
+    <li class="has-mega-menu">
+        <a
+            href="ProductLotList.html">PRODUCT</a>
+    </li>
+    <li class="has-mega-menu">
+            <a
+                href="Order_list.html">ORDER</a>
+    </li>
+    <li class="has-mega-menu">
+            <a
+                href="CustomerList.html">CUSTOMER</a>
+    </li>
+    <li class="has-mega-menu">
+            <a
+                href="EmployeeList.html">EMPLOYEE</a>
+    </li>
+    <li class="has-mega-menu">
+        <a
+            href="promotionslist.html">Promotions</a>
+    </li>
+    <li class="has-mega-menu">
+        <a
+            href="/paymentlist">Payment</a>
+    </li>`;
+    
+menubaritem2.innerHTML = `<nav class="main-nav main-nav4">
+    <ul>
+        <li class="menu-item-has-children">
+            <a
+                href="/">Home</a>
+        </li>
+        <li class="has-mega-menu">
+            <a
+                href="ProductLotList.html">PRODUCT</a>
+        </li>
+        <li class="has-mega-menu">
+                <a
+                    href="Order_list.html">ORDER</a>
+        </li>
+        <li class="has-mega-menu">
+                <a
+                    href="CustomerList.html">CUSTOMER</a>
+        </li>
+        <li class="has-mega-menu">
+                <a
+                    href="EmployeeList.html">EMPLOYEE</a>
+        </li>
+        <li class="has-mega-menu">
+            <a
+                href="promotionslist.html">Promotions</a>
+        </li>
+        <li class="has-mega-menu">
+            <a
+                href="/paymentlist">Payment</a>
+        </li></nav>`;
