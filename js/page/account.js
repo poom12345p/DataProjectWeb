3fcefc891d68624ac3549b19273d00009a180dfa
fsuriya <flukbanrong@gmail.com>
2019-12-09 10:54:44 +0700

    Update account.js

--- js/page/account.js
+++ js/page/account.js
@@ -1,26 +1,18 @@
 let list03 = document.querySelector('.account-login');
 let list04 = document.querySelector('.AccountInfo');
 let list05 = document.querySelector('.AccountInfo2');
-console.log(URL);
-var parsURL = URL.replace('http://localhost:9000','');
-console.log(parsURL);
 
 if (sessionStorage.getItem('USER')) {
     var username = JSON.parse(sessionStorage.getItem('USER'));
     }
     else
     {
-      if(parsURL == '/productslist' || parsURL == '/ProductLotList.html'){
-        console.log('this public');
-      }else{
         location.replace(location.origin + "/login");
-      }
-      
     }
 list03.innerHTML=`
                   <a>${username.jobTitle}</a>
-                  <a href="http://localhost:9000/employeeInfo?employeeNumber=${username.employeeNumber}">${username.firstName} ${username.lastName}</a>`;
+                  <a href="${location.origin}/employeeInfo?employeeNumber=${username.employeeNumber}">${username.firstName} ${username.lastName}</a>`;
 console.log(username.firstName);
 
-list04.innerHTML=`<a class="fa fa-user" href="http://localhost:9000/employeeInfo?employeeNumber=${username.employeeNumber}"> Account Info</a>`;
-list05.innerHTML=`<a class="fa fa-user" href="http://localhost:9000/employeeInfo?employeeNumber=${username.employeeNumber}"> Account Info</a>`;
+list04.innerHTML=`<a class="fa fa-user" href="${location.origin}/employeeInfo?employeeNumber=${username.employeeNumber}"> Account Info</a>`;
+list05.innerHTML=`<a class="fa fa-user" href="${location.origin}/employeeInfo?employeeNumber=${username.employeeNumber}"> Account Info</a>`;
